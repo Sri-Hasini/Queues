8a01a8a6335956c52d62cd02a80ae2401980346e
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-12-23 14:23:54 +0530

    Update 1.pq_(max_heap).cpp

--- 1.pq_(max_heap).cpp
+++ 1.pq_(max_heap).cpp
@@ -1,6 +1,7 @@
 // priority queue is a type of queue where we can store elements in descending (by default) or ascending order.
 // we can only access top().
-// we can perform operations like pq.empty() says if it is empty or not, pq.top() returns the element at the top, pq.push() pushes element at the end , pq.pop() pops the element at beginning
+// we can perform operations like pq.empty() says if it is empty or not, pq.top() returns the element at the top, 
+// pq.push() pushes element at the end , pq.pop() pops the element at beginning
 // pq.swap() Used to swap the contents of two queues provided the queues must be of the same type, although sizes may differ.
 // we can write it in 2 ways min heap and max heap
 // the following is the ----------------------------------------------------MAX HEAP----------------------------------------------------------------------------------------------------------
